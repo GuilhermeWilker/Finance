7730d7f9978b6ec24c8f6da8ee96bd550861248b
Guilherme Wilker Santos Silva Lima <gwlima@rainhadapaz.local>
2024-04-03 13:41:17 -0300

    feat: Recuperando os dados da localStorage, na HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,26 @@
 <script setup>
 import Dashboard from '../components/Dashboard.vue'
+import { ref, onMounted } from 'vue'
+
+const transactions = ref([])
+
+onMounted(() => {
+  const storedTransactions = localStorage.getItem('transactions')
+
+  if (storedTransactions) {
+    transactions.value = JSON.parse(storedTransactions)
+  }
+})
 </script>
 
 <template>
   <main>
     <h1>Dashboard 📊</h1>
 
-    <Dashboard />
+    <div v-for="transaction in transactions" key="index">
+      <p>{{ transaction.type }} | {{ transaction.value }}</p>
+    </div>
+
+    <!-- <Dashboard /> -->
   </main>
 </template>
