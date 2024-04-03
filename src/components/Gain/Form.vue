de7d5c0a656a8d9d674af70fd4927997b259a46a
Guilherme Wilker <wilkerguilherme0@gmail.com>
2024-04-02 21:46:49 -0300

    feat: salvando as transações no localstorage

--- src/components/Gain/Form.vue
+++ src/components/Gain/Form.vue
@@ -1,84 +1,79 @@
 <template>
-    <form action="" class="form">
-      <legend>Adicione uma nova entrada</legend>
-  
-      <div class="form-group">
-        <label for="tipo-transacao">Tipo de Transação</label>
-        <select v-model="option">
-          <option value="gain">Ganho</option>
-          <option value="income">Despesa</option>
-        </select>
-      </div>
-  
-      <div class="form-group">
-        <label for="">Valor:</label>
-        <input type="text" v-model="valor" placeholder="R$01,00" />
-      </div>
-  
-      <button type="submit" @click.prevent="submitForm">
-        Cadastrar Entrada
-      </button>
-    </form>
-  </template>
-  
-  <script setup>
-  import { ref, reactive } from 'vue'
-  
-  const option = ref('gain')
-  const valor = ref('')
+  <form action="" class="form">
+    <legend>Adicione uma nova entrada</legend>
 
-  const transaction = reactive({
+    <div class="form-group">
+      <label for="tipo-transacao">Tipo de Transação</label>
+      <select v-model="option">
+        <option value="gain">Ganho</option>
+        <option value="expense">Despesa</option>
+      </select>
+    </div>
+
+    <div class="form-group">
+      <label for="">Valor:</label>
+      <input type="text" v-model="valor" placeholder="R$ 0,00" />
+    </div>
+
+    <button type="submit" @click.prevent="submitForm">Cadastrar Entrada</button>
+  </form>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const option = ref("gain");
+const valor = ref("");
+const transactions = ref([]);
+
+const submitForm = () => {
+  const transaction = {
     type: option.value,
-    value: valor.value
-  })
-  
-  const submitForm = () => {
-    const db = localStorage.setItem('transaction', JSON.stringify(
-        transaction(option.value, valor.value)
-        ))
-    
-  }
-  </script>
-  
-  <style scoped>
-  .form {
-    margin: 2em auto;
+    value: valor.value,
+  };
+  transactions.value.push(transaction);
+  localStorage.setItem("transactions", JSON.stringify(transactions.value));
 
-    border: 1px solid white;
-    padding: 1em;
-    border-radius: .3em;
-    width: 70%;
-  }
-  
-  .form-group {
-    width: 100%;
-    margin-block: .8em;
-  }
-  
-  .form-group label {
-    display: block;
-    font-size: .9em;
-  }
-  
-  .form-group input,
-  .form-group select {
-    width: 100%;
-    padding: .4em .8em;
-  }
+  option.value = "gain";
+  valor.value = "";
+};
+</script>
 
-  .form button {
-    width: 100%;
-    padding: .7em;
+<style scoped>
+.form {
+  margin: 2em auto;
+  border: 1px solid white;
+  padding: 1em;
+  border-radius: 0.3em;
+  width: 70%;
+}
 
-    border: 1px solid #fff;
-    background-color: rgb(52, 52, 224);
-    color: white;
+.form-group {
+  width: 100%;
+  margin-block: 0.8em;
+}
 
-    cursor: pointer;
-  }
+.form-group label {
+  display: block;
+  font-size: 0.9em;
+}
 
-  .form button:hover {
-    background-color: rgb(36, 36, 175);
-  }
-  </style>
-  
+.form-group input,
+.form-group select {
+  width: 100%;
+  padding: 0.4em 0.8em;
+}
+
+.form button {
+  width: 100%;
+  padding: 0.7em;
+  border: 1px solid #fff;
+  background-color: rgb(52, 52, 224);
+  color: white;
+  cursor: pointer;
+}
+
+.form button:hover {
+  background-color: rgb(36, 36, 175);
+}
+</style>
